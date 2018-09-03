c1d8cf3701a4d664df2be8cf0240b20d54ac1a58
Rashika Patel <rashika_patel@hotmail.com>
2018-09-03 15:57:51 +0100

    test attempt

---
+++ jasmine/spec/fizzBuzzSpec.js
@@ -0,0 +1,16 @@
+describe("FizzBuzz", function() {
+		var f1;
+	// beforeEach(function() {
+	// 	fizzbuzz = new FizzBuzz();
+	// });
+
+	it("should return Fizz for multiples of 3", function() {
+		f1 = new FizzBuzz();
+		expect(f1.hello).toEqual "Hello world";
+	});
+
+
+
+
+
+})
