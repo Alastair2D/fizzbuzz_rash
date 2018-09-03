0983d54c95cb602947cb7009751d2753b3c292af
Alastair Edmonds <alastair.edmonds@gmail.com>
2018-09-03 16:43:34 +0100

    finish fizzbuzz in javascript

--- jasmine/spec/fizzBuzzSpec.js
+++ jasmine/spec/fizzBuzzSpec.js
@@ -1,16 +1,27 @@
 describe("FizzBuzz", function() {
-		var f1;
-	// beforeEach(function() {
-	// 	fizzbuzz = new FizzBuzz();
-	// });
+		var fizzBuzz;
 
-	it("should return Fizz for multiples of 3", function() {
-		f1 = new FizzBuzz();
-		expect(f1.hello).toEqual "Hello world";
+	beforeEach(function() {
+		fizzBuzz = new FizzBuzz();
 	});
 
+	it('returns 1 when given 1 as an argument', function() {
+		expect(fizzBuzz.play(1)).toEqual(1);
+	});
 
+	// it('returns the input number when not a multiple of 3 and/or 5', function() {
+	// 	expect(fizzBuzz.play(num)).toEqual(num);
+	// });
+	
+	it("should return Fizz for multiples of 3", function() {
+		expect(fizzBuzz.play(3)).toEqual("Fizz");
+	});
+	
+	it("should return Buzz for multiples of 5", function() {
+		expect(fizzBuzz.play(5)).toEqual("Buzz");
+	});
 
-
-
+	it("should return FizzBuzz for multiples of 3 and 5", function() {
+		expect(fizzBuzz.play(15)).toEqual("FIZZBUZZ");
+	});
 })
--- jasmine/src/fizzBuzz.js
+++ jasmine/src/fizzBuzz.js
@@ -1,7 +1,18 @@
 function FizzBuzz() {
-
 }
 
-FizzBuzz.prototype.hello = function() {
-	return "Hello world"
+// FizzBuzz.prototype.play = function(num) {
+// 	return 1
+// 	};
+
+FizzBuzz.prototype.play = function(num) {
+	if (num % 15 == 0) {
+		return "FIZZBUZZ";
+	} else if (num % 3 == 0) {
+		return 'Fizz';	
+	} 
+	else if (num % 5 == 0) {
+		return 'Buzz';
+	}
+	else return (num); 
 };
